bce8f9879e82681f2b4c5401d93f1fb85db438ab
Anteneh <anttesdev@gmail.com>
2024-12-11 16:45:21 +0000

    3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
-function handleProfileSignup() {
+export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
     .then(([photoResponse, userResponse]) => {
       console.log(photoResponse.body, userResponse.firstName, userResponse.lastName);
     })
-    .catch(error => {
-      console.error('Signup system offline');
+    .catch(() => {
+      console.log('Signup system offline');
     });
 }
