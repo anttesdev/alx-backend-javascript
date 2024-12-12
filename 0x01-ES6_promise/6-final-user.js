f6d524e2b0d9d411e277731d4efe3a84fd0b3707
Anteneh <anttesdev@gmail.com>
2024-12-12 13:44:03 +0000

    6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,5 +1,5 @@
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
   return (await Promise.allSettled([
@@ -10,4 +10,3 @@ export default async function handleProfileSignup(firstName, lastName, fileName)
     value: result.status === 'fulfilled' ? result.value : result.reason,
   }));
 }
-
