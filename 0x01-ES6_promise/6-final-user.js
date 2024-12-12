7ea57c1f869f1f9e0e6185a0918ba8b6de60af51
Anteneh <anttesdev@gmail.com>
2024-12-12 13:31:27 +0000

    6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,8 +4,8 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.all([
     signUpUser(firstName, lastName),
-    uploadPhoto(fileName)
+    uploadPhoto(fileName),
   ])
-  .then(results => results.map(result => ({ status: 'fulfilled', value: result })))
-  .catch(error => [{ status: 'rejected', value: error }]));
+    .then((results) => results.map((result) => ({ status: 'fulfilled', value: result })))
+    .catch((error) => [{ status: 'rejected', value: error }]);
 }
